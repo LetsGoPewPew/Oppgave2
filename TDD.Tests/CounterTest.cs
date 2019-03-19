ce45143e2b35999dedae79e5735adbeb808603f9
stian D <tricknshit@hotmail.com>
2019-03-19 17:37:14 +0100

    Changed tests to use TestCaseSource for testing multiple numbers in a array instead of specifying each testcase above each test.

--- TDD.Tests/CounterTest.cs
+++ TDD.Tests/CounterTest.cs
@@ -11,31 +11,32 @@ namespace TDD.Tests
     [TestFixture]
     public class CounterTest
     {
-        [TestCase(33, "Hi")]
-        [TestCase(66, "Hi")]
-        [TestCase(99, "Hi")]
-        public void Divisable_by_three_returns_Hi(int input, string expected)
+        private static int[] divisableByThreeTestSource = { 33, 66, 99 };
+        private static int[] divisableByFiveTestSource = { 25, 65, 95 };
+        private static int[] divisableByThreeAndFiveTestSource = { 30, 60, 90 };
+
+        [Test]
+        [TestCaseSource("divisableByThreeTestSource")]
+        public void Divisable_by_three_returns_Hi(int input)
         {
             string result = Program.ConvertNumberToCorrectString(input);
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual("Hi", result);
         }
 
-        [TestCase(25, "Of")]
-        [TestCase(65, "Of")]
-        [TestCase(95, "Of")]
-        public void Divisable_by_five_returns_Of(int input, string expected)
+        [Test]
+        [TestCaseSource("divisableByFiveTestSource")]
+        public void Divisable_by_five_returns_Of(int input)
         {
             string result = Program.ConvertNumberToCorrectString(input);
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual("Of", result);
         }
-
-        [TestCase(30, "HiOf")]
-        [TestCase(60, "HiOf")]
-        [TestCase(90, "HiOf")]
-        public void Divisable_by_three_and_five_returns_HiOf(int input, string expected)
+        
+        [Test]
+        [TestCaseSource("divisableByThreeAndFiveTestSource")]
+        public void Divisable_by_three_and_five_returns_HiOf(int input)
         {
             string result = Program.ConvertNumberToCorrectString(input);
-            Assert.AreEqual(expected, result);
+            Assert.AreEqual("HiOf", result);
         }
     }
 }
