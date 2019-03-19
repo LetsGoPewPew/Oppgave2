a4d1062101b0ff0608aa6c7dcd08f1738e434e07
stian D <tricknshit@hotmail.com>
2019-03-19 16:37:46 +0100

    Add project files.

---
+++ TDD.Tests/CounterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TDD;
+
+namespace TDD.Tests
+{
+    [TestFixture]
+    public class CounterTest
+    {
+        [Test]
+        public void Same_output_as_input()
+        {
+            int input = 1;
+            Program.ConvertNumberToCorrectString(input);
+            Assert.AreEqual(2, input);
+        }
+    }
+}
---
+++ TDD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public class Program
+    {
+        public static readonly int start = 1;
+        public static readonly int end = 100;
+        public static StringBuilder stringBuilder = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+
+            Counter(start, end, stringBuilder);
+            Console.Write(stringBuilder);
+            Console.ReadLine();
+            
+
+        }
+        public static void Counter(int start, int end, StringBuilder stringBuilder)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                stringBuilder.AppendLine(ConvertNumberToCorrectString(i));
+            }
+        }
+        public static String ConvertNumberToCorrectString(int i)
+        {
+            return i.ToString();
+        }
+
+    }
+}
